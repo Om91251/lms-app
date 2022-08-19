4daf832983dd28c9c16937da8b473742c14d8064
chandan <chandansutradhar2@gmail.com>
2022-08-19 17:40:22 +0530

    added angular code

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+const apiServerIpAddress='172.20.20.196';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-client';
+}
